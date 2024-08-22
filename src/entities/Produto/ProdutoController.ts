7c99b7912455c8fa7c2808c5b68417e21fec31ed
TEKSYSTEM\Ricardo Alves <ricardoalves@teksystem.com.br>
2024-08-22 12:56:56 -0300

    Criar CRUD de produtos

---
+++ src/entities/Produto/ProdutoController.ts
@@ -0,0 +1,127 @@
+import { Request, Response, NextFunction } from "express";
+import ApiResponse from "../../public/ApiResponse";
+import { Categoria, Prisma, Produto } from "@prisma/client";
+import '../../../prisma/PrismaSingleton';
+import InvalidArgumentException from "../../public/exceptions/InvalidArgumentException";
+import { prisma } from "../../../prisma/PrismaSingleton";
+
+type ProdutoDto = Omit<Produto, 'categoriaProduto'> & {categoria: Omit<Categoria, 'descricao'> & Partial<Pick<Categoria, 'descricao'>>};
+
+export default class ProdutoController {
+    constructor() {}
+
+    static validateNome(nome: string): void {
+        if(nome === '') {
+            throw new InvalidArgumentException("Nome do produto deve ser informado");
+        }
+    }
+
+    static validatePreco(preco: Prisma.Decimal): void {   
+
+        if(preco.lessThanOrEqualTo(0)) {
+           throw new InvalidArgumentException("Preço do produto deve ser um valor maior que zero");    
+        }
+    }
+
+    static async validateCategoria(categoriaId: number): Promise<void> {                       
+        await prisma.categoria.findUniqueOrThrow({where: {id: categoriaId}});
+    }
+
+    static async validate(produto: Omit<Produto, 'id'>): Promise<void> {
+        ProdutoController.validateNome(produto.nome);
+        ProdutoController.validatePreco(produto.preco);
+        await ProdutoController.validateCategoria(produto.categoriaProduto);        
+    }
+
+    async create(req: Request, res: Response<ApiResponse<ProdutoDto>>, next: NextFunction): Promise<void> {
+        const produto: Omit<Produto, 'id'> = {
+            nome: '',
+            descricao: '',
+            preco: new Prisma.Decimal(0),
+            imagem: '',
+            categoriaProduto: 0
+        };
+        try {                             
+            produto.nome = req.body.nome;
+            produto.descricao = req.body.descricao;          
+            produto.preco = new Prisma.Decimal(req.body.preco ? req.body.preco : 0);            
+            produto.imagem = req.body.imagem;
+            produto.categoriaProduto = req.body.categoria?.id | 0;            
+
+            await ProdutoController.validate(produto);
+            const apiResponse: ApiResponse<ProdutoDto> = new ApiResponse<ProdutoDto>();
+            apiResponse.setData(await prisma.produto.create({
+                data: produto, 
+                omit: {categoriaProduto: true}, 
+                include: {categoria: true}}));
+            res.status(201).send(apiResponse);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async find(req: Request, res: Response<ApiResponse<ProdutoDto[]>>, next: NextFunction): Promise<void> {
+        try {                  
+            const apiResponse: ApiResponse<ProdutoDto[]> = new ApiResponse<ProdutoDto[]>();
+            apiResponse.setData(await prisma.produto.findMany({
+                omit: {categoriaProduto: true},
+                include: {categoria: true},                               
+                where: {id: {gt: 0}}
+            }));
+
+            res.status(200).send(apiResponse);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async findById(req: Request, res: Response<ApiResponse<ProdutoDto>>, next: NextFunction): Promise<void> {
+        try {
+            const apiResponse: ApiResponse<ProdutoDto> = new ApiResponse<ProdutoDto>();
+            apiResponse.setData(await prisma.produto.findUniqueOrThrow({                
+                where: {id: parseInt(req.params.id)},
+                omit: {categoriaProduto: true},
+                include: {categoria: true}
+            }));
+
+            res.status(200).send(apiResponse);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async update(req: Request, res: Response<ApiResponse<ProdutoDto>>, next: NextFunction): Promise<void> {
+        try {
+            const produto: Produto = await prisma.produto.findUniqueOrThrow({where: {id: parseInt(req.params.id)}});
+            
+            produto.nome = req.body.nome;
+            produto.descricao = req.body.descricao;
+            produto.preco = new Prisma.Decimal(req.body.preco || 0);
+            produto.imagem = req.body.imagem;
+            produto.categoriaProduto = req.body.categoria?.id || 0;
+
+            await ProdutoController.validate(produto);
+
+            const apiResponse: ApiResponse<ProdutoDto> = new ApiResponse<ProdutoDto>;
+            apiResponse.setData(await prisma.produto.update({
+                data: produto,
+                where: {id: produto.id},
+                omit: {categoriaProduto: true},
+                include: {categoria: true}
+            }));
+            
+            res.status(200).send(apiResponse);        
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async delete(req: Request, res: Response<ApiResponse<{}>>, next: NextFunction): Promise<void> {
+        try {            
+            await prisma.produto.delete({where: {id: parseInt(req.params.id)}});
+            res.status(200).send(new ApiResponse<{}>());
+        } catch (error) {
+            next(error);            
+        }
+    }
+}
