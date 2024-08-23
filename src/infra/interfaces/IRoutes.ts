459ca4dfdc07a5a6c33b6adca3d5761ad29ac0fe
TEKSYSTEM\Ricardo Alves <ricardoalves@teksystem.com.br>
2024-08-23 13:28:43 -0300

    Exigir metodo para ajustar body no formato da entidade

--- src/infra/interfaces/IRoutes.ts
+++ src/infra/interfaces/IRoutes.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
 
 export default interface IRoutes {
     register(): void;
     router: express.Router; 
     url: string;
+    bodyParaEntidade(req: Request, res: Response, next: NextFunction): void;
 }
